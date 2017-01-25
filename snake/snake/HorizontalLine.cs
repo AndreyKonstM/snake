7cd36a6d24238ada43a5b5e75ec8b9b6aa896156
AndreyKonstM <mikhaylik_andrey@mail.ru>
2017-01-25 18:45:03 +0300

    Классы линий

---
+++ snake/snake/HorizontalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace snake
+{
+    class HorizontalLine
+    {
+        List<point> plist;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            plist = new List<point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                point p = new point(x, y, sym);
+                plist.Add(p);
+            }
+
+           
+        }
+
+        public void Drow()
+        {
+            foreach (point p in plist)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
---
+++ snake/snake/VerticalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace snake
+{
+    class VerticalLine
+    {
+        List<point> plist;
+
+        public VerticalLine(int xTop, int xButton, int x, char sym)
+        {
+            plist = new List<point>();
+            for (int y = xTop; y <= xButton; y++)
+            {
+                point p = new point(x, y, sym);
+                plist.Add(p);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach (point p in plist)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
